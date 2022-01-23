6a3af797789230815f73d88a71fb0364185ec15a
thaong91 <tnguye13@chicagobooth.edu>
2022-01-23 05:08:57 +0000

    finish homework

--- string_case.rb
+++ string_case.rb
@@ -1,9 +1,11 @@
 # Output
-# 
+#
 # "HELLO friends AnD FaMiLy"
-# 
+#
 # using a combination of the upcase, downcase, and swapcase methods.
 
 greeting = "hello"
 people = " FRIENDS"
 other_people = " aNd fAmIlY"
+
+p greeting.upcase + " " + people.strip.downcase + " " + other_people.strip.swapcase
